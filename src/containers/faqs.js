a0e6e716f3803853f2c01f83388aecbea5513646
gauravraj0510 <62883388+gauravraj0510@users.noreply.github.com>
2020-12-02 13:21:15 +0530

    opt form completed

--- src/containers/faqs.js
+++ src/containers/faqs.js
@@ -15,12 +15,12 @@ export function FaqsContainer() {
         ))}
       </Accordion.Frame>
 
-      {/* <OptForm>
+      <OptForm>
         <OptForm.Input placeholder="Email address" />
         <OptForm.Button>Try it now</OptForm.Button>
         <OptForm.Break />
         <OptForm.Text>Ready to watch? Enter your email to create or restart your membership.</OptForm.Text>
-      </OptForm> */}
+      </OptForm>
     </Accordion>
   );
 }
