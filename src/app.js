f27d0c31ed9f7272ad7510b5f4eada8e446bb906
gauravraj0510 <62883388+gauravraj0510@users.noreply.github.com>
2020-12-02 12:09:45 +0530

    modules fixed and errors removed

--- src/app.js
+++ src/app.js
@@ -1,20 +1,7 @@
 import React from 'react';
-import jumboData from './fixtures/jumbo'
-import Jumbotron from './components/jumbotron'
+import {JumbotronContainer} from './containers/jumbotron';
 
 export default function App() {
-  return (
-    <Jumbotron.Container>
-      {jumboData.map((item) => (
-        <Jumbotron key={item.id} direction={item.direction}>
-          <Jumbotron.Pane>
-            <Jumbotron.Title>{item.title}</Jumbotron.Title>
-            <Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
-          </Jumbotron.Pane>
-          <Jumbotron.Image src={item.image} alt={item.alt}/>
-        </Jumbotron>
-      ))}
-    </Jumbotron.Container>
-  );
+  return <JumbotronContainer/>;
 }
 
