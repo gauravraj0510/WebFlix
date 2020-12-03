a8763f94f537d6be0ac24e7f3879db5033ce2007
gauravraj0510 <62883388+gauravraj0510@users.noreply.github.com>
2020-12-03 16:03:47 +0530

    profile icon display done

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,5 +1,12 @@
-import React  from 'react';
+import React, { useState, useContext} from 'react';
+import { SelectProfileContainer } from './profiles'
+import { FirebaseContext } from '../context/firebase';
 
 export function BrowseContainer({ slides}){
-    return <p>Hello</p>
+    const [profile, setProfile] = useState({});
+    const [loading, setLoading] = useState(true);
+    const { firebase } = useContext(FirebaseContext);
+    const user = firebase.auth().currentUser || {};
+    
+    return <SelectProfileContainer user={user} setProfile={setProfile}/>
 }
