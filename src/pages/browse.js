b69193a534d90df1c4e510ec67d2034bdfd84ecf
gauravraj0510 <62883388+gauravraj0510@users.noreply.github.com>
2020-12-03 14:17:02 +0530

    browse container added

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,5 +1,11 @@
 import React from 'react';
+import { BrowseContainer } from '../containers/browse';
+import { useContent } from '../hooks';
+import { selectionFilter } from '../utils';
 
-export default function Browse(){
-    return(<p>Hello Browse!!</p>);
+export default function Browse() {
+  const { series } = useContent('series');
+  const { films } = useContent('films');
+  const slides = selectionFilter({ series, films });
+  return <BrowseContainer slides={slides} />;
 }
