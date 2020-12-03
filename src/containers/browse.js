727862cdf14f2a60fce1df1904ad0787e9c4afa6
gauravraj0510 <62883388+gauravraj0510@users.noreply.github.com>
2020-12-03 22:00:50 +0530

    live search added

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -28,7 +28,16 @@ export function BrowseContainer({ slides }) {
     setSlideRows(slides[category]);
   }, [slides, category]);
 
+  useEffect(() => {
+    const fuse = new Fuse(slideRows, { keys: ['data.description', 'data.title', 'data.genre'] });
+    const results = fuse.search(searchTerm).map(({ item }) => item);
 
+    if (slideRows.length > 0 && searchTerm.length > 3 && results.length > 0) {
+      setSlideRows(results);
+    } else {
+      setSlideRows(slides[category]);
+    }
+  }, [searchTerm]);
 
   return profile.displayName ? (
     <>
