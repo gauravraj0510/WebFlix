90fd356704951b4b72b6d3fbc68a0b5fb30e0a37
gauravraj0510 <62883388+gauravraj0510@users.noreply.github.com>
2020-12-03 12:01:16 +0530

    firebase connection complete

--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -7,12 +7,12 @@ import 'firebase/auth';
 
 //Config here
 const config ={
-    apiKey: "",
-    authDomain: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: ""
+    apiKey: "AIzaSyC4iSSW4OKLOOa8AdaRURsc_z9wgBWnC5A",
+    authDomain: "webflix-51be8.firebaseapp.com",
+    projectId: "webflix-51be8",
+    storageBucket: "webflix-51be8.appspot.com",
+    messagingSenderId: "601081488808",
+    appId: "1:601081488808:web:4b405c1c0000983dca699d"
 };
 
 const firebase = Firebase.initializeApp(config);
